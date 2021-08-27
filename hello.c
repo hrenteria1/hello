a821085362950b6b65394a57fc5a268f28847973
Gage Witcig <gwitcig2@huskers.unl.edu>
2021-08-27 20:05:11 +0000

    added Henry Renteria

--- hello.c
+++ hello.c
@@ -1,16 +1,18 @@
 /**
  * Author: Henry Renteria
  * Date: 2021/27/08
- * 
+ *
  * A Simple program stating my name
- * 
+ *
  */
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
-     
+
     printf("Henry Renteria/n");
-     
+
+    printf("Gage Witcig here!!!\nComputer Science major i guess\n");
+
     return 0;
  }
