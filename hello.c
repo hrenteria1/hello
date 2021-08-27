22f3bb11257c5dfe5129498d89bf47f4f9e4ecb7
Henry Renteria <hrenteria2@huskers.unl.edu>
2021-08-27 18:14:35 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Henry Renteria
+ * Date: 2021/27/08
+ * 
+ * A Simple program stating my name
+ * 
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+    printf("Henry Renteria/n");
+     
+    return 0;
+ }
